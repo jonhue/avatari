1d7f5d5a5536c855a7e4b9a461a866b4e909f0e3
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-01-08 07:39:01 +0100

    minor bugfixess

--- app/helpers/avatari/avatar_helper.rb
+++ app/helpers/avatari/avatar_helper.rb
@@ -5,7 +5,7 @@ module Avatari
             if object.avatar?
                 image_tag object.avatar.url, class: 'avatari avatari--' + version.to_s
             elsif !object.class.avatari_initials.nil?
-                render partial: 'avatari/avatar', object: object, version: version.to_s
+                render partial: 'avatari/avatar', locals: { object: object, version: version.to_s }
             else
                 false
             end
--- avatari.gemspec
+++ avatari.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |gem|
     gem.add_dependency 'activerecord', '>= 5.0'
     gem.add_dependency 'activesupport', '>= 5.0'
 
+    gem.add_development_dependency 'rake', '~> 12.3'
     gem.add_development_dependency 'rspec', '~> 3.7'
     gem.add_development_dependency 'rubocop', '~> 0.52'
 end
