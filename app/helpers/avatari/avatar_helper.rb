cb91b76bc564f9b7627073016c78b788c03987f7
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-01-08 06:39:13 +0100

    minor bugfixes

--- app/helpers/avatari/avatar_helper.rb
+++ app/helpers/avatari/avatar_helper.rb
@@ -4,7 +4,7 @@ module Avatari
         def avatari object, size = :medium
             if object.avatar?
                 image_tag object.avatar.url, class: 'avatari ' + (size ? 'avatari--' + size.to_s : '')
-            elsif !object.avatari_initials.nil?
+            elsif !object.class.avatari_initials.nil?
                 render partial: 'avatari/avatar', object: object, size: size.to_s
             else
                 false
